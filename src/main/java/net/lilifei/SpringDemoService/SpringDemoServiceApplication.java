34c940e0dcb66d9379188f4a6955242cae1e56ac
Lifei Li <lupuswere@gmail.com>
2020-10-17 16:59:49 -0700

    First commit with Spring Boot skeleton code

---
+++ src/main/java/net/lilifei/SpringDemoService/SpringDemoServiceApplication.java
@@ -0,0 +1,13 @@
+package net.lilifei.SpringDemoService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/lilifei/SpringDemoService/SpringDemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lilifei.SpringDemoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
