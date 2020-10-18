0b8a1a7023c4e9b12006e01e5cc14039c52bb0da
Lifei Li <lupuswere@gmail.com>
2020-10-17 17:26:33 -0700

    Add first GET API as ping
    NOTE: this returns only a String, not a JSON.

---
+++ src/main/java/net/lilifei/SpringDemoService/controller/PingController.java
@@ -0,0 +1,13 @@
+package net.lilifei.SpringDemoService.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Service is healthy";
+    }
+}
