baa4693bf49f4b312ff344a17acc8bc06378dd75
Lifei Li <lupuswere@gmail.com>
2020-10-17 23:03:28 -0700

    Add customized configurations to ObjectMapper

--- src/main/java/net/lilifei/SpringDemoService/spring/ObjectMapperConfig.java
+++ src/main/java/net/lilifei/SpringDemoService/spring/ObjectMapperConfig.java
@@ -1,6 +1,12 @@
 package net.lilifei.SpringDemoService.spring;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +15,11 @@ public class ObjectMapperConfig {
 
     @Bean
     ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return new ObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .configure(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS, false)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule())
+                .registerModule(new GuavaModule());
     }
 }
