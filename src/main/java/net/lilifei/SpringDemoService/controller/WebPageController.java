8de3ea56b4bdf70d1a7203bb2bcbf5ca10783a34
Lifei Li <lupuswere@gmail.com>
2020-10-18 22:39:34 -0700

    Return HTML page

---
+++ src/main/java/net/lilifei/SpringDemoService/controller/WebPageController.java
@@ -0,0 +1,13 @@
+package net.lilifei.SpringDemoService.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebPageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
