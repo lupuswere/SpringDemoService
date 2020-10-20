3e7b1901a856558216ffada8d0d0d7bb20c2dab6
Lifei Li <lupuswere@gmail.com>
2020-10-19 18:32:37 -0700

    Don't cache the page

--- src/main/java/net/lilifei/SpringDemoService/controller/WebPageController.java
+++ src/main/java/net/lilifei/SpringDemoService/controller/WebPageController.java
@@ -3,11 +3,17 @@ package net.lilifei.SpringDemoService.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class WebPageController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(final HttpServletResponse httpServletResponse) {
+        // Set standard HTTP/1.1 no-cache headers.
+        httpServletResponse.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
+        // Set standard HTTP/1.0 no-cache header.
+        httpServletResponse.setHeader("Pragma", "no-cache");
         return "index.html";
     }
 }
