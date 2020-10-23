14c7cc2f8beefc4d8fbbd9a4a9aee53f8be93eae
Lifei Li <lupuswere@gmail.com>
2020-10-22 23:38:27 -0700

    Separate config for Mysql

--- src/main/java/net/lilifei/SpringDemoService/spring/StorageConfig.java
+++ src/main/java/net/lilifei/SpringDemoService/spring/StorageConfig.java
@@ -1,41 +1,20 @@
 package net.lilifei.SpringDemoService.spring;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
-
-import net.lilifei.SpringDemoService.model.Record;
 import net.lilifei.SpringDemoService.mysql.MysqlRecordStorage;
-import net.lilifei.SpringDemoService.mysql.RecordModelMapper;
 import net.lilifei.SpringDemoService.storage.RecordStorage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.sql.SQLException;
-
 @Configuration
 public class StorageConfig {
 
-    @Bean
-    MysqlDataSource mysqlDataSource() {
-        final MysqlDataSource mysqlDataSource = new MysqlDataSource();
-        mysqlDataSource.setServerName("localhost");
-        mysqlDataSource.setDatabaseName("demo");
-        mysqlDataSource.setUser("root");
-        mysqlDataSource.setPassword("");
-        try {
-            mysqlDataSource.setServerTimezone("UTC");
-        } catch (final SQLException e) {
-            throw new IllegalStateException(e);
-        }
-        return mysqlDataSource;
-    }
+    @Autowired
+    MysqlRecordStorage mysqlRecordStorage;
 
     @Bean
-    @Autowired
-    RecordStorage recordStorage(final MysqlDataSource mysqlDataSource) {
-        return MysqlRecordStorage.builder()
-                .mysqlDataSource(mysqlDataSource)
-                .modelMapper(Record.class, new RecordModelMapper())
-                .build();
+    RecordStorage recordStorage() {
+        return mysqlRecordStorage;
     }
 }
